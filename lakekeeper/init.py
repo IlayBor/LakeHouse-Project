39315da43ffc15279391efcb65eb74c19b7c17f8
Ilay Borshan <ilaybor2004@gmail.com>
2026-01-30 17:25:26 +0200

    mod(lakekeeper/init.py): added fault tolerance and logging

--- lakekeeper/init.py
+++ lakekeeper/init.py
@@ -1,20 +1,62 @@
 import requests
 import json
+import logging
 
 HOST = "lakekeeper"
+BASE_URL = f"http://{HOST}:8181"
 
-print("Accept TOS...")
-tos_response = requests.post(f"http://{HOST}:8181/management/v1/bootstrap", json={"accept-terms-of-use": True})
-print("Accepted!")
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-print("Creating Warehouse...")
-with open('warehouse.json', 'r') as f:
-    warehouse_data = json.load(f)
-response = requests.post(f"http://{HOST}:8181/management/v1/warehouse", json=warehouse_data)
-print("Created Warehouse!")
+def init():
+    # Accepting TOS
+    logging.info("Trying to accept TOS...")
+    try:
+        tos_response = requests.post(f"{BASE_URL}/management/v1/bootstrap", json={"accept-terms-of-use": True})
+        tos_response.raise_for_status()
+        logging.info("Accepted!")
+    except requests.exceptions.RequestException as err:
+        error_data = err.response.json()['error']
+        if error_data['type'] == 'CatalogAlreadyBootstrapped':
+            logging.warning("Boostrapped already, moving on...")
+            pass
+        else:
+            logging.error(f"Boostrap failure: {error_data['code']} {error_data['message']}")
+            return
 
-print("Creating Namespace...")
-print(response.json())
-warehouse_id = response.json()['warehouse-id']
-ns_response = requests.post(f"http://{HOST}:8181/catalog/v1/{warehouse_id}/namespaces", json={"namespace": ["default"]})
-print("Created Namespace!")
+    # Creating Warehouse
+    with open('warehouse.json', 'r') as f:
+        warehouse_data = json.load(f)
+    try:
+        logging.info("Creating Warehouse...")
+        warehouse_response = requests.post(f"{BASE_URL}/management/v1/warehouse", json=warehouse_data)
+        warehouse_response.raise_for_status()
+        warehouse_id = warehouse_response.json()['warehouse-id']
+        logging.info("Created Warehouse!")
+    except requests.exceptions.RequestException as err:
+        error_data = err.response.json()['error']
+        if error_data['type'] == 'CreateWarehouseStorageProfileOverlap':
+            warehouse_response = requests.get(f"{BASE_URL}/management/v1/warehouse")
+            warehouse_id = warehouse_response.json()['warehouses'][0]['warehouse-id']
+            logging.warning("Warehouse already created, moving on...")
+            pass
+        else:
+            logging.error(f"Warehouse Creation Failure: {error_data['code']} {error_data['message']}")
+            return
+        
+    # Creating Namespace
+    try:
+        logging.info("Creating Namespace...")
+        ns_response = requests.post(f"{BASE_URL}/catalog/v1/{warehouse_id}/namespaces", json={"namespace": ["default"]})
+        ns_response.raise_for_status()
+        logging.info("Created Namespace!")
+    except requests.exceptions.RequestException as err:
+        error_data = err.response.json()['error']
+        if error_data['type'] == 'AlreadyExistsException':
+            logging.warning("Namespace already created for this warehouse, moving on...")
+            pass
+        else:    
+            logging.error(f"Namespace Creation Failure: {error_data['code']} {error_data['message']}")
+            return
+
+if __name__ == "__main__":
+    init()
