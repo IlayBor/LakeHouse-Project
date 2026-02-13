7c8447779832eaee3719d43cb0e98c895080d486
Ilay Borshan <ilaybor2004@gmail.com>
2026-02-13 23:09:49 +0200

    renamed cte

--- Airflow/dags/dbt_project/models/marts/Game/game_categories.sql
+++ Airflow/dags/dbt_project/models/marts/Game/game_categories.sql
@@ -3,11 +3,11 @@ WITH steam_games as (
     from {{ ref("stg_steam_api__game_details") }}
 ),
 
-pivot_game_categories as (
+game_categories as (
     select steam_app_id, category_id, category_desc
     from steam_games
     cross join unnest(categories) as t(category_id, category_desc)
 )
 
 select * 
-from pivot_game_categories
+from game_categories
