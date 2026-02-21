32955a2dd02db0e4db226f0a6db2934f8f44f3e0
Ilay Borshan <ilaybor2004@gmail.com>
2026-02-21 13:24:51 +0200

    changed the host

--- Airflow/dags/pipelines/common/connections.py
+++ Airflow/dags/pipelines/common/connections.py
@@ -1,19 +1,20 @@
-
 import pyiceberg
 from s3fs import S3FileSystem
 import pyiceberg.catalog.rest
 # from cosmos import ProfileConfig
 # from cosmos.profiles.trino import TrinoBaseProfileMapping
-from pathlib import Path
+# from pathlib import Path
+
+host = "10.0.0.97"
 
 catalog = pyiceberg.catalog.rest.RestCatalog(
     name="catalog_name",
-    uri="http://lakekeeper:8181/catalog",
+    uri=f"http://{host}:8181/catalog",
     warehouse="lakehouse_warehouse",
 )
 
 s3fs = S3FileSystem(
-    endpoint_url="http://minio:9000",
+    endpoint_url=f"http://{host}:9000",
     key="ilaybor",
     secret="24342434",
 )
@@ -31,3 +32,4 @@ s3fs = S3FileSystem(
 #         },
 #     ),
 # )
+
